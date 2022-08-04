f3fad38cf8c54dd55dc2f5f752b9b5aee39db170
ZZy979 <979481894@qq.com>
2022-08-05 02:22:06 +0800

    修改执行单元测试的函数，实现传递命令行参数和重定向标准输入/输出
    真是太难了！！！！！

---
+++ run_test.cmake
@@ -0,0 +1,11 @@
+# 运行测试，输出到指定的文件
+# cmake -P run_test.cmake <command> <output_file> <input_file> [<arg>...]
+foreach(i RANGE 6 ${CMAKE_ARGC})
+  list(APPEND ARGS ${CMAKE_ARGV${i}})
+endforeach()
+execute_process(
+  COMMAND ${CMAKE_ARGV3} ${ARGS}
+  OUTPUT_FILE ${CMAKE_ARGV4}
+  INPUT_FILE ${CMAKE_ARGV5}
+  COMMAND_ERROR_IS_FATAL ANY
+)
