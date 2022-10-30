585e79fc6e7c81e84cb400190e18f93482c85286
ZZy979 <979481894@qq.com>
2022-10-30 15:18:51 +0800

    第7章计算器v2将计算循环抽取成单独的函数

--- ch07/calculator_v2/main.cpp
+++ ch07/calculator_v2/main.cpp
@@ -8,28 +8,36 @@ using namespace std;
 const string prompt = "> ";     // prompt user input
 const string result = "= ";     // used to indicate that what follows is a result
 
+Token_stream ts(cin);
+Parser parser(ts);
+
+void calculate();
+
 /*
  * 简单计算器v2，支持+,-,*,/,()运算
  * 输入";"打印结果，输入"q"退出
  */
 int main() {
     try {
-        Token_stream ts(cin);
-        Parser parser(ts);
-        while (cin) {
-            cout << prompt;
-            Token t = ts.get();
-            while (t.kind == print)
-                t = ts.get();
-            if (t.kind == quit)
-                break;
-            ts.putback(t);
-            cout << result << parser.expression() << endl;
-        }
+        calculate();
         return 0;
     }
     catch (exception& e) {
         cerr << e.what() << endl;
         return 1;
+    }
+}
+
+// expression calculation loop
+void calculate() {
+    while (cin) {
+        cout << prompt;
+        Token t = ts.get();
+        while (t.kind == print)
+            t = ts.get();
+        if (t.kind == quit)
+            break;
+        ts.putback(t);
+        cout << result << parser.expression() << endl;
     }
 }
